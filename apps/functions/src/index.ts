69e49f912eba3fd48fc4f1e0018496d60397d860
Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>
2023-10-24 19:30:13 -0700

    function build broken

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,11 +1,18 @@
+import { logger } from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
 import express from "express";
-import * as functions from "firebase-functions";
 import { iLikeTurtles } from "@acme/shared";
 import { faker } from "@faker-js/faker";
+
+initializeApp();
 
 const app = express();
 
 app.get("*", (req, res) => {
+  logger.log("Hello from logger");
   res.send({
     hello: "world",
     randomName: faker.person.firstName(),
@@ -14,4 +21,20 @@ app.get("*", (req, res) => {
   });
 });
 
-export const server = functions.https.onRequest(app);
+app.post("/addName/:name", async (req, res) => {
+  const name = req.params.name;
+  logger.log(`Adding name '${name}' to database`);
+
+  const writeResult = await getFirestore()
+    .collection("names")
+    .add({ name: name });
+
+  res.json({ result: `Name with ID ${writeResult.id} added.` });
+});
+
+app.get("/getNames", async (req, res) => {
+  const snapshot = await getFirestore().collection("names").get();
+  return snapshot;
+});
+
+export const server = onRequest(app);
